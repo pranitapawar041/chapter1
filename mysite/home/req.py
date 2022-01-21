95d7ab8a24bbcfcbd54b2b036247af77440bfbf6
Pranita Pawar <pranitapawar041@gmail.com>
2022-01-21 10:11:52 +0530

    class update

--- mysite/home/req.py
+++ mysite/home/req.py
@@ -1,11 +1,45 @@
-import requests
-r = requests.get('https://images.app.goo.gl/gWEx1jLczSxwdqcm7')
-print(r.text)
-print(r.status_code)
+def put_file(req: HttpRequest):
+    print(req.body)
+    filename = "out.txt" + str(random.random())
+    with open(filename, 'wb') as output:
+        output.write(req.body)
+    resp = {
+        "success": True,
+        "filename": filename
+    }
+    return JsonResponse(resp)
+def get_file(req: HttpRequest, filename):
+    print(filename)
+    with open("uploads/ " + filename, 'rb') as f:
+        content = f.read()
+        print(content)
+        return HttpResponse(content)
 
-url = 'https://images.app.goo.gl/gWEx1jLczSxwdqcm7'
-data = {
-    "sam" : 4,
-    "rahul": 5
-}
-r2 = requests.post(url=url, data=data)
+def delete_file(req:HttpRequest,filename):
+    print(filename)
+    if os.path.isfile(filename):
+        os.remove(filename)
+    deleted =  HttpResponse("file deleted")
+    return deleted
+
+# Get the path of current working directory
+pathname = os.getcwd()
+print(pathname)
+def list_file(req:HttpRequest):
+    dir_list = os.listdir(pathname) 
+    dir_list =HttpResponse(dir_list)
+    return dir_list
+
+
+urlpatterns = [
+    path(" ", views.index, name = 'home'),
+    path("index",views.index, name = "index"),
+    path("header", views.header, name = 'header'),
+    path("details",views.details, name = 'details'),
+    path("end", views.end, name = 'end'),
+    path('data', put_file),
+    path('uploads/data/<filename>', get_file),
+    path('uploads/data/<filename>',delete_file),
+    path('uploads',list_file)
+
+]
--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -14,43 +14,50 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from fileinput import filename
-from ssl import ALERT_DESCRIPTION_DECODE_ERROR
+from http.client import HTTPResponse
+from nturl2path import pathname2url
+import pathlib
 from django.http import HttpResponse, HttpRequest, JsonResponse
 from django.contrib import admin
-from django.urls import path
+from django.urls import URLPattern, path
+from django.views import View
 from home import views
 import random
 import os
-def put_file(req: HttpRequest):
-    print(req.body)
-    filename = "out.txt" + str(random.random())
-    with open(filename, 'wb') as output:
-        output.write(req.body)
-    resp = {
+import urllib.request
+import random
+class details(View):
+    url = 'http://example.com/'
+    def get(self,request,url):
+        req = urllib.Request(url)#give url here then call 
+        content = urllib.urlopen(req)
+        data = content.read()
+        content.close()
+        return HTTPResponse(url)
+    def put(self,url):
+        req = urllib.Request(url)#give url here then call 
+        content = urllib.urlopen(req)
+        data = content.write(req.body)
+        print(data)
+        resp = {
         "success": True,
-        "filename": filename
-    }
-    return JsonResponse(resp)
-def get_file(req: HttpRequest, filename):
-    print(filename)
-    with open("uploads/ " + filename, 'rb') as f:
-        content = f.read()
-        print(content)
-        return HttpResponse(content)
-
-def delete_file(req:HttpRequest,filename):
-    print(filename)
-    if os.path.isfile(filename):
-        os.remove(filename)
-    deleted =  HttpResponse("file deleted")
-    return deleted
+        "filename": URLPattern
+        }
+        return JsonResponse(resp)
+    def delete(self,url):
+         file_delete = pathlib.Path(url)
+         file_delete.unlink()
+         deleted = HttpResponse("delete success!")
+         return deleted
 
 # Get the path of current working directory
-pathname = os.getcwd()
-def list_file(req:HttpRequest):
-    dir_list = os.listdir(pathname) 
-    dir_list =HttpResponse(dir_list)
-    return dir_list
+    pathname = os.getcwd()
+    print(pathname)
+    def list_file(req:HttpRequest):
+        dir_list = os.listdir(pathname2url) 
+        dir_list =HttpResponse(",".join(dir_list))
+        return dir_list
+
 
 urlpatterns = [
     path(" ", views.index, name = 'home'),
@@ -58,9 +65,10 @@ urlpatterns = [
     path("header", views.header, name = 'header'),
     path("details",views.details, name = 'details'),
     path("end", views.end, name = 'end'),
-    path('data', put_file),
-    path('uploads/data/<filename>', get_file),
-    path('uploads/data/<filename>',delete_file),
-    path('uploads',list_file)
+    path('data', details.put),
+    path('uploads/data/<filename>', details.get),
+    path('deleted/data/<filename>',details.delete),
+    path('uploads',details.list_file)
+
 ]
 
